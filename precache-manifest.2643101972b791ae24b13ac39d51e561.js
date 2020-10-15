84006507da1a6a0df5f9f7b3d82e82a142bd5a82
Ricardo Sifontes <jrsifontes16@gmail.com>
2020-10-14 22:24:06 -0600

    Updates

---
+++ precache-manifest.2643101972b791ae24b13ac39d51e561.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19482f13de58aa8b78de070103ca778",
+    "url": "/reactjs-landing-page/index.html"
+  },
+  {
+    "revision": "a919e9584f1747008a67",
+    "url": "/reactjs-landing-page/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "cafd67700623306e93fe",
+    "url": "/reactjs-landing-page/static/css/main.281af3ab.chunk.css"
+  },
+  {
+    "revision": "a919e9584f1747008a67",
+    "url": "/reactjs-landing-page/static/js/2.d35aa77b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactjs-landing-page/static/js/2.d35aa77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafd67700623306e93fe",
+    "url": "/reactjs-landing-page/static/js/main.6f024e09.chunk.js"
+  },
+  {
+    "revision": "43f778162f5abb4fa180",
+    "url": "/reactjs-landing-page/static/js/runtime-main.6b559678.js"
+  },
+  {
+    "revision": "7bcf2e7aae06e49d29e2a253d78e6e7f",
+    "url": "/reactjs-landing-page/static/media/curly_img.7bcf2e7a.svg"
+  },
+  {
+    "revision": "2f738682c71060ad00ed95db9d1b74c0",
+    "url": "/reactjs-landing-page/static/media/hero_hamburger.2f738682.svg"
+  },
+  {
+    "revision": "0e08f7ad935b0117bf83d00732592ad3",
+    "url": "/reactjs-landing-page/static/media/hero_hamburger_menu.0e08f7ad.png"
+  },
+  {
+    "revision": "2467c9f6168883ff26fbb82cda6e0dbe",
+    "url": "/reactjs-landing-page/static/media/hero_yellow_img.2467c9f6.svg"
+  },
+  {
+    "revision": "c6eea6915a4c2f2fca8270efa98eb5fe",
+    "url": "/reactjs-landing-page/static/media/map1.c6eea691.jpg"
+  },
+  {
+    "revision": "2808efb09a7603072c57e9f41f429a2c",
+    "url": "/reactjs-landing-page/static/media/play_store.2808efb0.jpg"
+  },
+  {
+    "revision": "f34d3d2f4dff8d626502f6cade3e0613",
+    "url": "/reactjs-landing-page/static/media/red_splash_1.f34d3d2f.svg"
+  },
+  {
+    "revision": "1b499b6487a7257d898fbd778f55f90b",
+    "url": "/reactjs-landing-page/static/media/red_splash_2.1b499b64.svg"
+  },
+  {
+    "revision": "282bd0fc136e31727a3f334e6d4704b6",
+    "url": "/reactjs-landing-page/static/media/section2_img.282bd0fc.jpg"
+  },
+  {
+    "revision": "6b7367dd144127c3de94b0db10180002",
+    "url": "/reactjs-landing-page/static/media/smartphone.6b7367dd.png"
+  },
+  {
+    "revision": "812c148bd087ab48f34c31c1b9b163be",
+    "url": "/reactjs-landing-page/static/media/testimonials_bg_ketchup.812c148b.png"
+  }
+]);
